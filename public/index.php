2109a62c24228428b5901437ba9d91d5840af039
welkenspinheiro <welkenspinheiro@gmail.com>
2023-05-22 10:47:45 -0300

    how outout buffer work

--- public/index.php
+++ public/index.php
@@ -1,1 +1,15 @@
-<h1>Home</h1>
+<?php
+
+$data = ['name'=>'Welkens'];
+
+ob_start();
+
+extract($data);
+
+require './home.php';
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+var_dump($content);
